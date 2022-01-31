6a68e024d0a861b9d3c476f1cef8556bd22442be
Christian Garcia <christian.garcia@cwservices.org>
2022-01-31 14:36:23 -0800

    SAMS and HATS Functions working GUI is complete
    Missing Drag and drop function, will look into implementation. If end user's do not need it then I will remove it.

--- Vituix Converter.py
+++ Vituix Converter.py
@@ -1,10 +1,30 @@
-import pandas as pd
-import PyQt5
-from PyQt5 import QtWidgets, uic
+import os
+import shutil
 import sys
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+import pandas as pd
+from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+
 from vituix_converter_UI import (Ui_MainWindow)
+
+
+def no_file_selected():
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Warning)
+    msg.setWindowIcon(QtGui.QIcon("D:/Python Projects/HATS SAMS Conversion/Icon/Vituix_Converter_Icon_R1.ico"))
+    msg.setText("No File Was Selected!")
+    msg.setWindowTitle("Warning!")
+    retval = msg.exec_()
+
+
+def process_completed():
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Information)
+    msg.setWindowIcon(QtGui.QIcon("D:/Python Projects/HATS SAMS Conversion/Icon/Vituix_Converter_Icon_R1.ico"))
+    msg.setText("Files are converted.")
+    msg.setWindowTitle("Completed!")
+    retval = msg.exec_()
 
 
 class MyMainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -12,11 +32,76 @@ class MyMainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
 
-        self.pushButton_hats.clicked.connect(hats_conversion)
+        self.pushButton_hats.clicked.connect(self.hats_conversion)
+        self.pushButton_sams.clicked.connect(self.sams_conversion)
         self.show()
 
     def hats_conversion(self):
 
+        hats_file = QtWidgets.QFileDialog.getOpenFileName(self, "Select HATS File", "", 'txt (*.txt)')[0]
+
+        if hats_file == '':
+            no_file_selected()
+        else:
+
+            hats_data = pd.read_csv(hats_file, sep="\t", skiprows=3, header=None, on_bad_lines='skip')
+
+            txt_name = []
+
+            for i in range(1, 148, 2):
+                txt_name.append(hats_data.loc[0, i])
+
+            hats_data = hats_data.drop([0, 1])
+            hats_data = hats_data.reset_index(drop=True)
+            # Select folder to save data output
+            save_folder = QtWidgets.QFileDialog.getExistingDirectory(self, "Select HATS Save Folder")
+            # iteration variable for lists in following for loop
+            i = 1
+            # iterate over all text names, allocate corresponding data, and then save as txt
+            for j in range(0, len(txt_name)):
+                test_df_1 = hats_data[0].values.tolist()
+                test_df_2 = hats_data[i].values.tolist()
+                test_df_3 = hats_data[i + 1].values.tolist()
+
+                export_df = pd.DataFrame(
+                    {'0': test_df_1,
+                     '1': test_df_2,
+                     '2': test_df_3
+                     })
+
+                save_file = save_folder + '/' + txt_name[j] + '.txt'
+                export_df.to_csv(save_file, sep='\t', index=False, na_rep='NaN', header=False)
+                i = i + 2
+
+            process_completed()
+
+    def sams_conversion(self):
+
+        sams_file = QtWidgets.QFileDialog.getExistingDirectory(self, "Select SAMS Folder")
+
+        if sams_file == '':
+            no_file_selected()
+        else:
+            output_folder = QtWidgets.QFileDialog.getExistingDirectory(self, "Select/Create Save Folder")
+
+            if output_folder == '':
+                no_file_selected()
+
+            else:
+
+                file_names = []
+                for x in os.listdir(sams_file):
+                    if x.endswith(".txt"):
+                        file_names.append(x)
+
+                for i in range(len(file_names)):
+                    newPath = shutil.copy(os.path.join(sams_file, file_names[i]), output_folder)
+
+                    sams_data = pd.read_csv(newPath, sep="\t", skiprows=5, header=None, on_bad_lines='skip')
+
+                    sams_data.to_csv(newPath, sep='\t', index=False, na_rep='NaN', header=False)
+
+                process_completed()
 
 
 if __name__ == "__main__":
